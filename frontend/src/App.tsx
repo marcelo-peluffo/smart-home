8da441db13bfe750463acaf5e0b7f58970116851
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-06-29 11:57:31 -0500

    create working API button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,14 @@
-import { useState } from 'react';
+import APIButton, { type API } from './components/APIButton';
 
 export default function App() {
-  const talkToServer = async () => {
-    setResponse('Loading...');
-    
-    try {
-      const response = await fetch('http://pi.local:3000');
-      setResponse(await response.text());
-
-    } catch (error) {
-      console.log(error);
-      setResponse('Error connecting to Pi');
-    }
-
-  };
-
-  const resetResponse = () => {
-    setResponse('No Response');
-  };
-
-  const [response, setResponse] = useState('No Response');
+  const api: API = {
+    url: 'http://pi.local:3000',
+    result: null
+  }
 
   return (
     <>
-      <button onClick={talkToServer}>Response: {response}</button>
-      <button onClick={resetResponse}>Reset Response</button>
+      <APIButton APIData={api} label={'GET /'}></APIButton>
     </>
   );
 }
---
+++ frontend/src/components/APIButton.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+export type API = {
+    url: string
+    result: any
+}
+
+type APIButtonProps = {
+    APIData: API
+    label: string
+}
+
+
+export default function APIButton({ APIData, label }: APIButtonProps) {
+    const [buttonText, setButtonText] = useState(label)
+
+    const callAPI = async () => {
+        try {
+            const response = await fetch(APIData.url)
+            const result = await response.json()
+
+            APIData.result = result
+            const stringAPI: string = JSON.stringify(result)
+
+            setButtonText(stringAPI)
+
+        } catch (error) {
+            console.error('Failed to fetch at: ', APIData.url)
+            setButtonText('Failed request. Please try again.')
+        }
+    }
+
+    return (
+        <>
+            <button onClick={callAPI}>{buttonText}</button>
+        </>
+    )
+}

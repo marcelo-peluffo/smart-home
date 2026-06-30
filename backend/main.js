42a2d221d0af3964caf489935bed33715109f3e1
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-06-30 13:32:51 -0500

    make /status/dog-eat GET and POST endpoints

--- backend/main.js
+++ backend/main.js
@@ -3,10 +3,27 @@ const app = express();
 const cors = require('cors');
 const port = 3000;
 
+let hasDogEaten = false
+
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send({'Peluffo\'s Smart Home': 'Welcome to the Smart Home API!'});
+});
+
+// when this endpoint is called, update backend state 
+app.post('/api/dog-eat', (req, res) => {
+    hasDogEaten = true;
+})
+
+app.get('/status/dog-eat', (req, res) => {
+    if (hasDogEaten) {
+        res.send({'dog-eat': 'true'});
+        hasDogEaten = false
+    }
+    else {
+        res.send({'dog-eat': 'false'})
+    }
 });
 
 app.listen(port, () => {
